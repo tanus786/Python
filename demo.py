8c1aca544ce13ed71e9facd5fdd1a7bbc668ef32
Tanu soni <Tanusoni2312@gmail.com>
2023-09-20 00:55:20 +0530

    umm

--- demo.py
+++ demo.py
@@ -4,6 +4,19 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 
 Window.clearcolor = (1,1,1,1)
+
+class ButtonApp(App):
+    def build(self):
+        btn = Button(text="Hello",size_hint=(0.2,0.2),pos_hint={"center_x":0.5 , "center_y":0.5},font_size="20sp",on_press=self.btn_click)
+        return btn
+    
+    def btn_click(self,btn):
+        print("Button Clicked")
+        
+ButtonApp().run() 
+
+
+
 # class DemoApp(App):
 #     def build(self):
 #         label = Label(text="Hello World",font_size="120sp",italic = True,color=(1,0,0,1))
@@ -12,9 +25,3 @@ Window.clearcolor = (1,1,1,1)
     
 # DemoApp().run() 
 
-class ButtonApp(App):
-    def build(self):
-        btn = Button(text="Hello")
-        return btn
-
-ButtonApp().run() 
