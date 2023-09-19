8edcb573201390d4eee5bbf13c274b331cb0f37e
Tanu soni <Tanusoni2312@gmail.com>
2023-09-20 00:36:37 +0530

    Lebel And Buttons

--- demo.py
+++ demo.py
@@ -1,11 +1,20 @@
 from kivy.app import App 
 from kivy.uix.label import Label
+from kivy.uix.button import Button 
 from kivy.core.window import Window
 
 Window.clearcolor = (1,1,1,1)
-class DemoApp(App):
-    def build(self):
-        label = Label(text="Hello World",font_size="120sp",italic = True,color=(1,0,0,1))
-        return label
+# class DemoApp(App):
+#     def build(self):
+#         label = Label(text="Hello World",font_size="120sp",italic = True,color=(1,0,0,1))
+#         btn = Button(text="Hello")
+#         return label
     
-DemoApp().run() 
+# DemoApp().run() 
+
+class ButtonApp(App):
+    def build(self):
+        btn = Button(text="Hello")
+        return btn
+
+ButtonApp().run() 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
- 
+class DemoApp(App):
+#     def build(self):
+#         label = Label(text="Hello World",font_size="120sp",italic = True,color=(1,0,0,1))
+#         btn = Button(text="Hello")
+#         return label
+    
+# DemoApp().run() 
